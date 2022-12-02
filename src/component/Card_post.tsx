dd6272b91209938251b45e60820f58493a9caef1
Satang Budsai <Satang_budsai@cmu.ac.th>
2022-12-02 15:08:15 +0700

    +component Card

--- src/component/Card_post.tsx
+++ src/component/Card_post.tsx
@@ -1,46 +1,15 @@
 import { Fragment, useState } from "react";
+import Card from "./Card";
 
 export default function Card_post() {
   return (
     <Fragment>
       <div className="p-5 hide-card">
         <div className="grid md:grid-cols-2 grid-col-1 gap-4">
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
-            </div>
-          </div>
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
-            </div>
-          </div>
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
-            </div>
-          </div>
-          <div className="card bg-primary text-primary-content">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
-            </div>
-          </div>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
     </Fragment>
